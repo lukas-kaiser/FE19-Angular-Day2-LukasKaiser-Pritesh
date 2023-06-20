68a9e10afebb5a09169280072a6e20d6df4de7b5
Lukas Kaiser <lukas.kaiser@kununu.com>
2023-06-20 14:46:30 +0200

    add adding function

---
+++ Day2/src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+counter : number = 0;
+
+countClick() {
+  this.counter += 10;
+}
+}
